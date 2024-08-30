127fbac99a57dd1bb170e110a47d4cf9df4f6610
suru-ach <srujanacharya36@gmail.com>
2024-08-30 19:54:33 +0530

    add message transmission

--- src/main/java/ServerLogger.java
+++ src/main/java/ServerLogger.java
@@ -1,3 +1,8 @@
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.net.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 // Shared resource
 class Buffer {
@@ -36,10 +41,10 @@ class Buffer {
             }
         }
         full--;
-        notify();
-        int temp = index_second;
+        int idx = index_second;
         index_second = (index_second + 1) % capacity;
-        return array[temp];
+        notify();
+        return array[idx];
     }
 }
 
@@ -53,10 +58,49 @@ class UDPClient implements Runnable {
         thread.start();
     }
 
+    public boolean udp_send(String message, int acknowledgement) throws IOException {
+        final int port = 8080;
+        final int responseSize = 1024;
+        byte[] responseString = new byte[responseSize];
+
+        DatagramSocket socket = new DatagramSocket();
+        //Send data over
+        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), InetAddress.getLocalHost(), port);
+        socket.send(packet);
+
+        DatagramPacket responsePacket = new DatagramPacket(responseString, responseSize);
+        socket.receive(responsePacket);
+        String responseMessage = new String(responseString, 0, responsePacket.getLength());
+        System.out.println(responseMessage);
+        System.out.println("Ack: "+acknowledgement);
+
+        /*
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if()
+            }
+        }, 2000);
+         */
+
+        return false;
+    }
+
     public void run() {
         while(true) {
-            String response = buffer.get();
+            String ackMessage = Integer.toHexString(buffer.index_second);
+            if(ackMessage.length() == 1) ackMessage = "0" + ackMessage;
+            String response = ackMessage + buffer.get();
             System.out.println(response);
+            // udp send and wait for acknowledgement
+            try {
+                while (udp_send(response,Integer.valueOf(ackMessage, 16))) {
+                }
+            } catch(IOException exc) {
+                System.out.println("Socket exception " + exc.getMessage());
+                return;
+            }
         }
     }
 }
@@ -77,7 +121,7 @@ class Producer implements Runnable {
                 // logic to write into
 
                 // Test
-                Thread.sleep(100);
+                Thread.sleep(1000);
                 String message = "Hello "+System.currentTimeMillis()+" "+this.thread.getName();
                 buffer.put(message);
             } catch(InterruptedException exc) {
@@ -91,7 +135,7 @@ class Producer implements Runnable {
 public class ServerLogger {
     public static void main(String[] args) {
         // Test
-        Buffer buffer = new Buffer(10);
+        Buffer buffer = new Buffer(17);
         Producer producer = new Producer(buffer);
         UDPClient client = new UDPClient(buffer);
     }
